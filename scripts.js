e363480bdab524e233c6d2c4444e3c022e628618
Rene Pacifique Mutanguha <PACIFIQUEGIT>
2024-10-09 21:00:50 +0200

    last tasks

--- scripts.js
+++ scripts.js
@@ -1,7 +1,41 @@
 let profileMenu = document.getElementById("profileMenu");
+let profileMenuN = document.getElementById("profileMenuN");
+let profileMenuA = document.getElementById("profileMenuA");
+let spanhtml = document.getElementById("spanhtml");
+let alljobsdv = document.getElementById("alljobsdv");
+let alljobs = document.getElementById("alljobs");
+let fullscreen = document.getElementById("fullscreen");
 
 function toggleMenu(){
     profileMenu.classList.toggle("open-menu");
+}
+
+function toggleMenuN(){
+    profileMenuN.classList.toggle("open-menu");
+}
+
+function toggleMenuA(){
+    profileMenuA.classList.toggle("open-menu");
+    if(profileMenuA.classList.contains("open-menu")){
+        spanhtml.innerHTML = "See less";
+    }
+    else{
+        spanhtml.innerHTML = "See more...";
+    }
+}
+
+function toggleMenuJ(){
+    alljobsdv.classList.toggle("open-menu");
+    if(alljobsdv.classList.contains("open-menu")){
+        alljobs.innerHTML = "Show less";
+    }
+    else{
+        alljobs.innerHTML = "Show all";
+    }
+}
+
+function toggledisplay(){
+    fullscreen.classList.toggle("open-menu");
 }
 
 let sideActivity = document.getElementById("sidebarActivity");
